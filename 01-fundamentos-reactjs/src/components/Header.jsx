4eb9a58892e392d12e6ae9025a9f4a86c7667d84
João Vitor de Sá Medeiros Santos <jvsms13@gmail.com>
2022-08-21 18:39:20 -0300

    header

--- 01-fundamentos-reactjs/src/components/Header.jsx
+++ 01-fundamentos-reactjs/src/components/Header.jsx
@@ -1,8 +1,10 @@
 import style from './Header.module.css'
+
+import igniteLogo from '../assets/ignite-logo.svg'
 export function Header() {
     return (
         <header className={style.header}>
-            <strong>Um nome legal</strong>
+            <img src={igniteLogo} alt="Logotipo do Ignite" />
         </header>
     );
 }
