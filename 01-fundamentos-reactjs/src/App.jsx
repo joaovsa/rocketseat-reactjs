ef6103104664a3abbf4d463798825588383b96a2
João Vitor de Sá Medeiros Santos <jvsms13@gmail.com>
2022-08-21 19:37:48 -0300

    more stuff

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,20 +1,29 @@
 import { Header } from './components/Header'
 import { Post } from './Post'
 
+import './global.css'
+import styles from './App.module.css'
+
 function App() {
   return (
     <div>
       <Header />
-      <Post 
-        author="João" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde soluta totam nam impedit recusandae fugit accusamus ipsa, dolorem dolor id ducimus vero, voluptate perspiciatis molestiae magnam facilis, consequatur reprehenderit cumque!"
-      />
-      <Post 
-        author="Paulo Muzy" 
-        content="Nunca tomei bomba"
-      />
+      <div className={styles.wrapper}>
+        <aside>sidebar</aside>
+        <main>
+        <Post 
+          author="João" 
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde soluta totam nam impedit recusandae fugit accusamus ipsa, dolorem dolor id ducimus vero, voluptate perspiciatis molestiae magnam facilis, consequatur reprehenderit cumque!"
+        />
+        <Post 
+          author="Paulo Muzy" 
+          content="Nunca tomei bomba"
+        />
+        </main>
+      </div>
     </div>
   )
 }
 
 export default App
+
