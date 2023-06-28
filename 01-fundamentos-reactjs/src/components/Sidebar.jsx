fccacdd661e4ed84d4c2da57025c16f362ff135f
João Vitor de Sá <joao.sa@livance.com.br>
2023-06-28 00:14:06 -0300

    mudanças no sidebar

--- 01-fundamentos-reactjs/src/components/Sidebar.jsx
+++ 01-fundamentos-reactjs/src/components/Sidebar.jsx
@@ -1,18 +1,19 @@
 import styles from './Sidebar.module.css'
 
-export function Sidebar(){
-    return(
+export function Sidebar() {
+    return (
         <aside className={styles.sidebar}>
-            <img 
+            <img
                 className={styles.cover}
-                src="https://images.unsplash.com/photo-1566837945700-30057527ade0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" 
+                src="https://images.unsplash.com/photo-1566837945700-30057527ade0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
             />
 
             <div className={styles.profile}>
+                <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/11942940?v=4" />
                 <strong>João Vitor de Sá</strong>
                 <span>Web Developer</span>
             </div>
-        
+
             <footer>
                 <a href='#'>
                     Editar seu perfil
