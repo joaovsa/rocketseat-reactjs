42cec916f5c9152d7ef7d90d0fd83d7ddeaf94cb
João Vitor de Sá Medeiros Santos <jvsms13@gmail.com>
2024-01-27 17:53:58 -0300

    styling posts

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,9 +1,9 @@
 import { Header } from './components/Header'
-import { Post } from './Post'
+import { Post } from './components/Post'
+import { Sidebar } from './components/Sidebar'
 
 import './global.css'
 import styles from './App.module.css'
-import { Sidebar } from './components/Sidebar'
 
 function App() {
   return (
@@ -12,14 +12,14 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-        <Post 
-          author="João" 
-          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde soluta totam nam impedit recusandae fugit accusamus ipsa, dolorem dolor id ducimus vero, voluptate perspiciatis molestiae magnam facilis, consequatur reprehenderit cumque!"
-        />
-        <Post 
-          author="Paulo Muzy" 
-          content="Nunca tomei bomba"
-        />
+          <Post 
+            author="João" 
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde soluta totam nam impedit recusandae fugit accusamus ipsa, dolorem dolor id ducimus vero, voluptate perspiciatis molestiae magnam facilis, consequatur reprehenderit cumque!"
+          />
+          <Post 
+            author="Paulo Muzy" 
+            content="Nunca tomei bomba"
+          />
         </main>
       </div>
     </div>
---
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -0,0 +1,24 @@
+import styles from './Post.module.css';
+
+export function Post() {
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src= "https://avatars.githubusercontent.com/u/11942940?v=4"/> 
+                    <div className={styles.authorInfo}>
+                        <strong>João Vitor de Sá</strong>
+                        <span>Web Developer</span>
+                    </div>
+                </div>
+
+                <time title="27 de Janeiro às 17:08" dateTime='2024-01-27 17:08:00'>Publicado há 1h</time>
+            </header>
+
+            <div className={styles.content}>
+                <p>Oi, eu sou um bom programador, mas eu vou serei excelente!</p>
+                <p>Para mais informações, fale comigo ou acesse meu <a href='https://medium.com/@joovitordes/'>Medium</a></p>
+            </div>
+        </article>
+    )
+}
